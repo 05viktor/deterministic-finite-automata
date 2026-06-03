b7ca55f2180afeafdbbec19fc81cc5cde3edae7d
Catanescu Viktor <victorcatanescu@yahoo.com>
2026-06-03 14:46:26 +0300

    Comentarii

--- DFA_F.py
+++ DFA_F.py
@@ -1,7 +1,12 @@
 import automaton_parser as parser
 
+# DFA stands for deterministic finite automaton M is a 5-tuple, (Q, Σ, δ, q0, F), consisting of
+# a finite set of states Q
+# a finite set of input symbols called the alphabet Σ
+# a transition function δ : Q × Σ → Q
+# an initial (or start) state q0∈Q
+# a set of accepting (or final) states F⊆Q
 
-# DFA stands for deterministic finite automaton M is a 5-tuple, (Q, Sigma, delta, q0, F).
 
 
 def parseRules(rule_lines):
